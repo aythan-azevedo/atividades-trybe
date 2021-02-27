2b49334aec94445c24a10dee0a5ae117db0f8741
aythan azevedo <aythan16@gmail.com>
2021-02-27 00:10:32 -0300

    encerrar

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/jb.js
@@ -0,0 +1,39 @@
+/**let n = 8;
+let linha = ""
+let simb = "*"
+for(let i=0; i <n; i++){
+    linha = linha + simb 
+}
+
+for(let i=0; i <n ; i++){
+    console.log(linha)
+}**/
+console.log("------------------------------------------------------")
+//---------------------------------------
+/** let n2= 5;
+
+for(let i=0; i < n2; i++){
+    linha = linha + simb;
+    console.log(linha)
+}
+**/
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+let inputPosition = n;
+
+for(let i=0; i < n; i++){
+    for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+        if (columnIndex < inputPosition) {
+          inputLine = inputLine + ' ';
+        } else {
+          inputLine = inputLine + symbol;
+        }
+    }
+        console.log(inputLine);
+        inputLine = '';
+        inputPosition -- ;
+
+
+}
